e476a97f9ac2a1a36ba6b9d3c154950e75b70936
DerrickOngoma <Odrumz64@gmail.com>
2024-06-04 13:47:48 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        return [candidate for candidate in candidates if sorted(candidate) == sorted(self.word)]
+    
+
